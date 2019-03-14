29a7e8eb30db144bb1161276cc0a41d048cf3d2c
Mikkel O. Laursen <m@ikkel.dk>
2019-03-14 13:12:33 +0100

    Add right index.html template with all favicons

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,28 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Construction/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Construction from "./components/UnderConstruction.vue";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
-    HelloWorld
+    Construction
   }
-}
+};
 </script>
 
 <style>
+body {
+  margin: 0;
+  padding: 0;
+}
+
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
---
+++ src/components/UnderConstruction.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="container">
+    <h1>This site is under construction</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Construction"
+};
+</script>
+
+<style scoped>
+.container {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  height: calc(100vh - 60px);
+}
+
+h1::after {
+  content: "...";
+  color: yellow;
+}
+</style>
+
+
