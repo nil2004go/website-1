d62619b4a11cb085ec66245c456c456db7ab77a8
Mikkel O. Laursen <m@ikkel.dk>
2019-03-16 01:37:53 +0100

    Import mixins

--- src/main.js
+++ src/main.js
@@ -6,3 +6,9 @@ Vue.config.productionTip = false
 new Vue({
   render: h => h(App),
 }).$mount('#app')
+
+import titleMixin from './components/mixins/titleMixin'
+Vue.mixin(titleMixin)
+
+import loopTitleMixin from './components/mixins/loopTitleMixin'
+Vue.mixin(loopTitleMixin)
