0960090a2d53a54eb44a13519f5e4246cf9a0e20
Mikkel O. Laursen <m@ikkel.dk>
2019-03-16 22:39:40 +0100

    Add temperary resume page

--- src/components/CurriculumVitae.vue
+++ src/components/CurriculumVitae.vue
@@ -1,6 +1,12 @@
 <template>
   <div id="cv">
-    <p>Hej</p>
+    <div class="errorMsg">
+      <h1 class="error">Please view on a desktop computer</h1>
+    </div>
+
+    <div class="page" size="A4">
+      <h1>Curriculum Vitae</h1>
+    </div>
   </div>
 </template>
 
@@ -11,5 +17,36 @@ export default {
 </script>
 
 <style scoped>
+h1 .error {
+  margin: 70px;
+  font-size: 3em;
+}
+
+h1 {
+  padding: 70px;
+  font-size: 3em;
+}
+
+/* Remove shadows and margin on print */
+@media print {
+  body,
+  page {
+    margin: 0;
+    box-shadow: 0;
+  }
+}
+
+@media screen and (min-width: 801px) {
+  .errorMsg {
+    display: none;
+  }
+}
+
+/* Disable mobile users */
+@media screen and (max-width: 800px) {
+  .page {
+    display: none;
+  }
+}
 </style>
 
