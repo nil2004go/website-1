8ce438fe23e789dc10dd600eb4e8a31652cc1443
Mikkel O. Laursen <m@ikkel.dk>
2019-04-02 22:20:22 +0200

    Add no selection to UnderConstruction

--- src/views/UnderConstruction.vue
+++ src/views/UnderConstruction.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="container">
     <img src="../assets/dozer.png" id="dozer">
-    <h1>This site is under construction</h1>
+    <h1 class="noselect">This site is under construction</h1>
     <div id="dots">
       <span class="dot"></span>
       <span class="dot"></span>
@@ -19,6 +19,16 @@ export default {
 
 <style lang="scss" scoped>
 @import "../styles/_construction.scss";
+
+.noselect {
+  -webkit-touch-callout: none; /* iOS Safari */
+    -webkit-user-select: none; /* Safari */
+     -khtml-user-select: none; /* Konqueror HTML */
+       -moz-user-select: none; /* Firefox */
+        -ms-user-select: none; /* Internet Explorer/Edge */
+            user-select: none; /* Non-prefixed version, currently
+                                  supported by Chrome and Opera */
+}
 </style>
 
 
