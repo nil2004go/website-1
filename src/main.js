c9f6059f9952f95b23c7602dac3f5074aa98cda7
Mikkel O. Laursen <m@ikkel.dk>
2019-04-07 13:27:35 +0200

    Add favicon mixin

--- src/main.js
+++ src/main.js
@@ -1,28 +1,30 @@
-import Vue from 'vue'
-import router from './router'
-import vueHeadful from 'vue-headful'
-import VueAnalytics from 'vue-analytics'
-import VueKonami from 'vue-konami'
+import Vue from 'vue';
+import router from './router';
+import vueHeadful from 'vue-headful';
+import VueAnalytics from 'vue-analytics';
+import VueKonami from 'vue-konami';
 
-import App from './App.vue'
+import App from './App.vue';
 
-import titleMixin from './components/mixins/titleMixin'
-import loopTitleMixin from './components/mixins/loopTitleMixin'
+import titleMixin from './components/mixins/titleMixin';
+import loopTitleMixin from './components/mixins/loopTitleMixin';
+import faviconMixin from './components/mixins/faviconMixin.js';
 
 Vue.use(VueAnalytics, {
-  id: "UA-137498042-1",
-  router
-})
-Vue.use(VueKonami)
+	id: 'UA-137498042-1',
+	router
+});
+Vue.use(VueKonami);
 
-Vue.mixin(titleMixin)
-Vue.mixin(loopTitleMixin)
+Vue.mixin(titleMixin);
+Vue.mixin(loopTitleMixin);
+Vue.mixin(faviconMixin);
 
 Vue.component('vue-headful', vueHeadful);
 
 Vue.config.productionTip = false;
 
 new Vue({
-  router,
-  render: h => h(App),
-}).$mount('#app')
+	router,
+	render: (h) => h(App)
+}).$mount('#app');
