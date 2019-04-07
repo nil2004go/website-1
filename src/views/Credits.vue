7f6e52f83d8a7ed077be9d3652801907b5d69fda
Mikkel O. Laursen <m@ikkel.dk>
2019-04-07 21:53:10 +0200

    Add Credits page

---
+++ src/views/Credits.vue
@@ -0,0 +1,98 @@
+<template>
+  <div id="container">
+    <div class="entry" id="icons">
+      <div class="headline">
+        <img src="../../src/assets/pencil.png" alt="Pencils">
+        <h1>Icons</h1>
+      </div>
+      <div class="credit">
+        Icons made by
+        <a href="https://icon54.com/" title="Pixel perfect">Pixel perfect</a> from
+        <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by
+        <a
+          href="http://creativecommons.org/licenses/by/3.0/"
+          title="Creative Commons BY 3.0"
+          target="_blank"
+        >CC 3.0 BY</a>
+      </div>
+
+      <div class="credit">
+        Icons made by
+        <a href="https://www.flaticon.com/packs/interface" title="Chanut">Chanut</a> from
+        <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by
+        <a
+          href="http://creativecommons.org/licenses/by/3.0/"
+          title="Creative Commons BY 3.0"
+          target="_blank"
+        >CC 3.0 BY</a>
+      </div>
+
+      <div class="credit">
+        Icons made by
+        <a
+          href="https://www.flaticon.com/authors/vectors-market"
+          title="Vectors Market"
+        >Vectors Market</a> from
+        <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by
+        <a
+          href="http://creativecommons.org/licenses/by/3.0/"
+          title="Creative Commons BY 3.0"
+          target="_blank"
+        >CC 3.0 BY</a>
+      </div>
+
+      <div class="credit">
+        Icons made by
+        <a href="https://www.freepik.com/" title="Freepik">Freepik</a> from
+        <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by
+        <a
+          href="http://creativecommons.org/licenses/by/3.0/"
+          title="Creative Commons BY 3.0"
+          target="_blank"
+        >CC 3.0 BY</a>
+      </div>
+
+      <div>
+        Icons made by
+        <a
+          href="https://www.flaticon.com/authors/pixelmeetup"
+          title="Pixelmeetup"
+        >Pixelmeetup</a> from
+        <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by
+        <a
+          href="http://creativecommons.org/licenses/by/3.0/"
+          title="Creative Commons BY 3.0"
+          target="_blank"
+        >CC 3.0 BY</a>
+      </div>
+    </div>
+
+    <div class="entry" id="effects">
+      <div class="headline">
+        <img src="../../src/assets/coding.png" alt="Coding">
+        <h1>Effects</h1>
+      </div>
+      <div class="trick">
+        Link hover effect by
+        <a href="https://css-tricks.com/author/chriscoyier/">Chris Coyier</a> from
+        <a
+          href="https://css-tricks.com/full-width-containers-limited-width-parents/"
+        >CSS-Tricks</a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Credits",
+  favicon: "credits/favicon.ico",
+  title: "✪ I salute these people"
+};
+</script>
+
+<style lang="scss" scoped>
+@import "../styles/_credits.scss";
+</style>
+
+
